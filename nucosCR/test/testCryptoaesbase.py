436211f2a8ffe8a02a9967a58697bac5130bcb81
oliver <braun.o@phystec.de>
2016-10-19 16:26:02 +0200

    version number imported

--- nucosCR/test/testCryptoaesbase.py
+++ nucosCR/test/testCryptoaesbase.py
@@ -2,9 +2,11 @@ from __future__ import print_function
 import unittest
 import sys
 sys.path.append('../../')
-from nucosCR import CryptoAESBase, random
+from nucosCR import CryptoAESBase, random, version
+from nucosCR import read_from_file, write_to_file, get_crc_from_file
 import nucosCR
 print(nucosCR.__file__)
+print("VERSION: %s"%version)
 
 class UTestCryptonize(unittest.TestCase):
     @classmethod
